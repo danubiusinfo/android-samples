947564622c8211cba11287acc2e4ecfe2d10c7d5
Bence Ignacz <ignacz.bence@danubiusinfo.hu>
2022-07-22 19:22:31 +0200

    - Add initial readme - Finalize CreditCard app

--- DanubiusCreditCard/app/src/main/java/io/danubius/dicard/MainActivity.kt
+++ DanubiusCreditCard/app/src/main/java/io/danubius/dicard/MainActivity.kt
@@ -3,14 +3,13 @@ package io.danubius.dicard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.danubius.dicard.ui.CreditCard
@@ -28,37 +27,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiCardApp() {
+    val cards = listOf(
+        CardInfo(
+            backgroundDrawable = R.drawable.card_mesh,
+            providerDrawable = R.drawable.mc,
+            cardNumber = "8547 9658 6325 4521",
+            cardHolder = "John Fluffy"
+        ),
+        CardInfo(
+            backgroundDrawable = R.drawable.card_mesh_2,
+            providerDrawable = R.drawable.visa,
+            cardNumber = "6582 4521 3256 8522",
+            cardHolder = "Elisa Silverstone"
+        ),
+        CardInfo(
+            backgroundDrawable = R.drawable.card_mesh_3,
+            providerDrawable = R.drawable.visa,
+            cardNumber = "9856 7452 2569 7413",
+            cardHolder = "Salvador Dali"
+        )
+    )
     DanubiusCreditCardTheme {
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxHeight()
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            CreditCard(
-                cardInfo = CardInfo(
-                    backgroundDrawable = R.drawable.card_mesh,
-                    providerDrawable = R.drawable.mc,
-                    cardNumber = "8547 9658 6325 4521",
-                    cardHolder = "John Fluffy"
-                )
-            )
-            CreditCard(
-                cardInfo = CardInfo(
-                    backgroundDrawable = R.drawable.card_mesh_2,
-                    providerDrawable = R.drawable.visa,
-                    cardNumber = "6582 4521 3256 8522",
-                    cardHolder = "Elisa Silverstone"
-                )
-            )
-            CreditCard(
-                cardInfo = CardInfo(
-                    backgroundDrawable = R.drawable.card_mesh_3,
-                    providerDrawable = R.drawable.visa,
-                    cardNumber = "9856 7452 2569 7413",
-                    cardHolder = "Salvador Dali"
-                )
-            )
+            items(cards) { card ->
+                CreditCard(cardInfo = card)
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,12 @@
+# Android Samples
+
+This repository contains a set of Android sample projects. You can find the corresponding blog post for every projects below 👇
+
+Don't forget to Start this repo and visit our blog at https://danubius.io
+
+## Sample projects
+------------
+
+| Project | Description | Screenshot |
+|:-----|---------|---------|
+| Danubius Credit Card | Simple Credit Card showcase app | <img src="readme/screenshots/CreditCard_full_framed.png" width="320" alt="Danubius Credit Card"> |
